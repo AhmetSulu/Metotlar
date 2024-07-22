6d5f43a5723c1cb6415fc03fde6b726392c0c4e3
Ahmet Sulu <ahmet.sulu1993@gmail.com>
2024-07-22 10:15:06 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,35 @@
+﻿// Geriye Değer Döndürmeyen Bir void metot
+static void SarkıSozu()
+{
+    Console.WriteLine("Gerçek hayat, seni rehin alacak kendinden uzaklaştıracak.");
+}
+SarkıSozu();
+
+// Geriye Tamsayı Döndüren Bir metot
+
+static int RandomNumber()
+{
+    Random rastgele = new Random();
+    int sayi = rastgele.Next();
+    return sayi % 2;
+}
+int kalan = RandomNumber();
+Console.WriteLine("Rastgele sayının 2'ye bölümünden kalan: " + kalan);
+
+// Parametre Alan ve Geriye Değer Döndüren Bir Metot
+
+static int Product(int sayi1, int sayi2)
+{
+    return sayi1 * sayi2;
+}
+int sonuc = Product(8, 9);
+Console.WriteLine("Sayıların çarpımı: " + sonuc);
+
+
+// Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
+
+static void WelcomeDear(string isim, string soyisim)
+{
+    Console.WriteLine($"Hoş Geldiniz {isim} {soyisim}");
+}
+WelcomeDear("Ahmet", "Sulu");
